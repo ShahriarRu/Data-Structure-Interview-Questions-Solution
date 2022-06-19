306749e50f1d0061cd25810188f7c2b846291593
Shahriar Alif <shahriarcse26@gmail.com>
2022-06-20 01:47:51 +0600

    Linked List implementation in js

---
+++ LinkedListImplementation.js
@@ -0,0 +1,29 @@
+class MyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+let linkedList = new MyLinkedList(10);
+linkedList.append(6);
+linkedList.append(15);
+linkedList.append(20);
+linkedList.append(9);
+console.log(linkedList);
