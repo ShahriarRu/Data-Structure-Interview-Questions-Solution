aab293009bdcfff151fb5ca9b70a7bd9bf8bab0e
Shahriar Alif <shahriarcse26@gmail.com>
2022-06-20 19:13:57 +0600

    Linked Prepend method added

--- LinkedListImplementation.js
+++ LinkedListImplementation.js
@@ -19,6 +19,18 @@ class MyLinkedList {
     this.length++;
     return this;
   }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
 
 let linkedList = new MyLinkedList(10);
@@ -26,4 +38,6 @@ linkedList.append(6);
 linkedList.append(15);
 linkedList.append(20);
 linkedList.append(9);
+linkedList.prepend(12);
+linkedList.prepend(18);
 console.log(linkedList);
