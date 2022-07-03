db2f5576c29a94e5a908981996767537393f341f
Shahriar Alif <shahriarcse26@gmail.com>
2022-07-03 23:21:19 +0600

    niddle in the haystack

---
+++ strStr.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  hay_len = haystack.length;
+  need_len = needle.length;
+  map = {};
+  for (i = 0; i < hay_len; i += need_len) {
+    str = heystack.slice(i);
+  }
+};
