74d5d6da8511e087585fa52a0f7a7b4d260dd0fb
Shahriar Alif <shahriarcse26@gmail.com>
2022-07-06 19:23:39 +0600

    insert func added

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,17 +1,41 @@
 class Node {
-  constructor(value) {
-    this.left = null;
-    this.right = null;
-    this.value = value;
-  }
+	constructor(value){
+		this.left = null
+		this.right = null
+		this.value = value
+	}
 }
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-  insert(value) {}
-  lookup(value) {}
+class BinarySearchTree{
+	constructor(){
+		this.root = null
+	}
+	insert(value){
+		let newNode = new Node(value);
+		if(!this.root){
+			this.root = newNode;
+		}
+		let temp = this.root;
+		while(!temp){
+			if(value >= temp.value){
+				temp = temp.right
+			}
+			else if(value < temp.value){
+				temp = temp.left
+			}
+		}
+		temp = newNode;
+	}
+	
+	lookup(value){
+		
+	}
 }
 
-const tree = new BinarySearchTree();
+const tree = new BinarySearchTree()
+tree.insert(9)
+tree.insert(12)
+tree.insert(5)
+tree.insert(10)
+tree.insert(13)
+console.log(tree)
