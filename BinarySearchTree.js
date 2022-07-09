bcb0929f9c4262e8cac3d2aed7e848a86e5fd2a2
Shahriar Alif <shahriarcse26@gmail.com>
2022-07-09 16:26:45 +0600

    lookup function added

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,8 +1,8 @@
 class Node {
   constructor(value) {
+    this.value = value;
     this.left = null;
     this.right = null;
-    this.value = value;
   }
 }
 
@@ -37,17 +37,37 @@ class BinarySearchTree {
     }
   }
 
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return "Tree is empty";
+    }
+    let temp = this.root;
+    let level = 1;
+    while (temp) {
+      console.log(temp.value);
+      if (temp.value == value) {
+        return `found ${value} at level ${level}`;
+      } else if (temp.value < value) {
+        temp = temp.right;
+      } else {
+        temp = temp.left;
+      }
+      level++;
+    }
+    return "Item not found";
+  }
 }
 
 const tree = new BinarySearchTree();
-//tree.insert(9);
+tree.insert(9);
 tree.insert(4);
 tree.insert(6);
 tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+
+console.log(tree.lookup(170));
 
 function traverse(node) {
   const tree = { value: node.value };
