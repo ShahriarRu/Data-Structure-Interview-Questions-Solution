ae53336f2cf28f69a0c7cde8fc2e0d5b9d4b14c5
Shahriar Alif <shahriarcse26@gmail.com>
2022-07-10 06:59:10 +0600

    Graph implementation initiated

---
+++ Graph.js
@@ -0,0 +1,14 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjecentList = {};
+  }
+
+  addVertex(node) {}
+
+  addEdge(node1, node2) {}
+
+  showConnectons() {}
+}
+
+const myGraph = new Graph();
