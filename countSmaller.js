1c1ee618273bb7b35ddccd12856f3c9a13670b29
Shahriar Alif <shahriarcse26@gmail.com>
2022-07-23 20:48:23 +0600

    countsmaller solved

---
+++ countSmaller.js
@@ -0,0 +1,36 @@
+var countSmaller = function (nums) {
+  let bst = new BST(nums[nums.length - 1]);
+  let outputArr = new Array(nums.length).fill(0);
+  for (let j = nums.length - 2; j >= 0; j--) {
+    outputArr[j] = bst.insert(nums[j]);
+  }
+  return outputArr;
+};
+
+class BST {
+  constructor(v, s = 0) {
+    this.value = v;
+    this.left = null;
+    this.right = null;
+    this.size = s;
+  }
+
+  insert(v, s = 0) {
+    if (v > this.value) {
+      if (this.right == null) {
+        this.right = new BST(v);
+        return this.size + s + 1;
+      } else {
+        return this.right.insert(v, s + this.size + 1);
+      }
+    } else {
+      this.size++;
+      if (this.left == null) {
+        this.left = new BST(v);
+        return s;
+      } else {
+        return this.left.insert(v, s);
+      }
+    }
+  }
+}
