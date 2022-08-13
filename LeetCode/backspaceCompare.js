1d5d3d5873ad65f2b539fcdf7cf9103a68ef5560
Shahriar Alif <shahriarcse26@gmail.com>
2022-08-14 02:23:09 +0600

    compare backspace bruteforce

--- LeetCode/backspaceCompare.js
+++ LeetCode/backspaceCompare.js
@@ -39,3 +39,36 @@ var backspaceCompare = function (s, t) {
   }
   return true;
 };
+
+const buildString = function (string) {
+  const builtString = [];
+  for (let p = 0; p < string.length; p++) {
+    if (string[p] !== "#") {
+      builtString.push(string[p]);
+    } else {
+      builtString.pop();
+    }
+  }
+
+  return builtString;
+};
+
+var backspaceCompare = function (S, T) {
+  const finalS = buildString(S);
+  const finalT = buildString(T);
+
+  if (finalS.length !== finalT.length) {
+    return false;
+  } else {
+    for (let p = 0; p < finalS.length; p++) {
+      if (finalS[p] !== finalT[p]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+const string1 = "ab#z";
+const string2 = "az#z";
+console.log(backspaceCompare(string1, string2));
