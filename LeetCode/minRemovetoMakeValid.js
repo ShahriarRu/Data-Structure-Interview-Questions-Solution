632a1315c22f7cd35ced92e5e1af13532c7dcec0
Shahriar Alif <shahriarcse26@gmail.com>
2022-09-02 23:23:52 +0600

    MinRemoveToMakeValid solved

---
+++ LeetCode/minRemovetoMakeValid.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+function setCharAt(str, index, chr) {
+  if (index > str.length - 1) return str;
+  return str.substring(0, index) + chr + str.substring(index + 1);
+}
+
+var minRemoveToMakeValid = function (s) {
+  stack = [];
+  index = [];
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] == "(") {
+      stack.push("(");
+      index.push(i);
+    } else if (s[i] == ")" && stack.length == 0) {
+      index.push(i);
+    } else if (s[i] == ")") {
+      stack.pop();
+      index.pop();
+    }
+  }
+
+  for (var i = index.length - 1; i >= 0; i--) {
+    s = setCharAt(s, index[i], "");
+  }
+
+  return s;
+};
