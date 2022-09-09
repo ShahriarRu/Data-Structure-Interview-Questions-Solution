caa450c97fe171bfb7c439bcb2e2b5f7720f0cbd
Shahriar Alif <shahriarcse26@gmail.com>
2022-09-09 20:50:18 +0600

    searchRange optimized solution

--- LeetCode/searchRange.js
+++ LeetCode/searchRange.js
@@ -1,14 +1,51 @@
+// var searchRange = function (nums, target) {
+//   arr = [];
+//   ind = nums.indexOf(target);
+//   if (ind == -1) {
+//     return [-1, -1];
+//   }
+//   arr.push(ind);
+//   while (nums[ind] == target) {
+//     ind++;
+//   }
+//   arr.push(ind - 1);
+
+//   return arr;
+// };
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+function binSearch(arr, item) {
+  left = 0;
+  right = arr.length - 1;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (arr[mid] === item) {
+      return mid;
+    } else if (arr[mid] < item) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
 var searchRange = function (nums, target) {
-  arr = [];
-  ind = nums.indexOf(target);
+  ind = binSearch(nums, target);
   if (ind == -1) {
     return [-1, -1];
   }
-  arr.push(ind);
-  while (nums[ind] == target) {
-    ind++;
+  left = ind;
+  right = ind;
+  while (nums[left] == target || nums[right] == target) {
+    if (nums[left] == target) {
+      left--;
+    }
+    if (nums[right] == target) {
+      right++;
+    }
   }
-  arr.push(ind - 1);
-
-  return arr;
+  return [left + 1, right - 1];
 };
