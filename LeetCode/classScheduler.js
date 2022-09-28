6d010197622f29ff0d9f6ab4e859f7e96666f0ee
Shahriar Alif <shahriarcse26@gmail.com>
2022-09-28 23:59:04 +0600

    class scheduler

---
+++ LeetCode/classScheduler.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+
+let generateGraph = (prerequisites) => {
+    let graph = {}
+    for(let [x,y] of prerequisites){
+        if(!graph[x]){
+            graph[x] = []
+            graph[x].push(y)
+        }else{
+            graph[x].push(y)
+        }
+    }
+    return graph
+}
+
+let BFS = (numCourses, prerequisite) => {
+    let seen = []
+    let queue = []
+    queue.push(...
+    
+}
+var canFinish = function(numCourses, prerequisites) {
+    let graph = generateGraph(prerequisites)
+    
+};
