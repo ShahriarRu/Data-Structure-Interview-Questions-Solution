6d876f2252d27a4432bc39a5dc39f38f06da451f
Shahriar Alif <shahriarcse26@gmail.com>
2022-12-27 14:00:29 +0600

    reverse k nodes solved

--- C++/reverse_k_nodes.cpp
+++ C++/reverse_k_nodes.cpp
@@ -29,3 +29,35 @@ public:
         return v[0];
     }
 };
+
+class Solution {
+public:
+        ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head) return NULL;
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = head;
+        ListNode* pre = NULL;
+        ListNode* last_tail = dummy;
+        int num = 0;
+        while(cur){
+            cur = cur->next;
+            num++;
+        }
+        cur = head;
+        while(num >= k){
+            ListNode* tail = cur;
+            for(int i = 0; i < k; i++){
+                ListNode* temp = cur->next;
+                cur->next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            last_tail->next = pre;
+            last_tail = tail;
+            num -= k;
+        }
+        last_tail->next = cur;
+        return dummy->next;
+    }
+    
+};
